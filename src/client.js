7625c3d17eaadd89f739a5e1d3ed72256a19ca47
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-11-12 00:35:07 -0500

    Comment - Sanitize Tables
    Comment - Sanitize Tables

--- src/client.js
+++ src/client.js
@@ -3,4 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
     "https://lwnvwutsklnofoppxshk.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjY5MDY2NiwiZXhwIjoxOTUyMjY2NjY2fQ.6p7S9787WCBOcesJ3sWymR_RhndWA4R4vMuuEolnPaA"
+    // Sanitize tables
+    // https://supabase.io/docs/learn/auth-deep-dive/auth-row-level-security
 )
