c00ff53eb87d73eecdc6b0fec6ecd3001e49e882
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-11-11 23:39:02 -0500

    supabase-reactjs creation
    supabase-reactjs creation

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    "https://lwnvwutsklnofoppxshk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjY5MDY2NiwiZXhwIjoxOTUyMjY2NjY2fQ.6p7S9787WCBOcesJ3sWymR_RhndWA4R4vMuuEolnPaA"
+)
