fa748ec455046e4faf0a0550bf36fa839debc9ea
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-11-12 20:42:44 -0500

    Reverted to previous update
    Reverted to previous update

--- src/client.js
+++ src/client.js
@@ -1,5 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+    "https://lwnvwutsklnofoppxshk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjY5MDY2NiwiZXhwIjoxOTUyMjY2NjY2fQ.6p7S9787WCBOcesJ3sWymR_RhndWA4R4vMuuEolnPaA"
+    // Sanitize tables
+    // https://supabase.io/docs/learn/auth-deep-dive/auth-row-level-security
+)
